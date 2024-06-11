52fe5af3336aaf2406ff01c307bdd10e4521ae0c
Gleison Brito <brittobaptista93@gmail.com>
2024-06-11 15:02:37 -0300

    fix api

--- routes.py
+++ routes.py
@@ -22,30 +22,11 @@ def init_routes(app):
         alunos = load_json(alunos_file)
         return jsonify(alunos), 200
 
-    @app.route('/alunos/<int:id>', methods=['PUT'])
-    def update_aluno(id):
-        data = request.get_json()
-        alunos = load_json(alunos_file)
-        aluno = next((a for a in alunos if a['id'] == id), None)
-        if aluno is None:
-            return jsonify({"message": "Aluno não encontrado!"}), 404
-
         aluno['nome'] = data['nome']
         aluno['idade'] = data['idade']
         aluno['curso_id'] = data.get('curso_id')
         save_json(alunos_file, alunos)
         return jsonify({"message": "Aluno atualizado com sucesso!"}), 200
-
-    @app.route('/alunos/<int:id>', methods=['DELETE'])
-    def delete_aluno(id):
-        alunos = load_json(alunos_file)
-        aluno = next((a for a in alunos if a['id'] == id), None)
-        if aluno is None:
-            return jsonify({"message": "Aluno não encontrado!"}), 404
-
-        alunos.remove(aluno)
-        save_json(alunos_file, alunos)
-        return jsonify({"message": "Aluno deletado com sucesso!"}), 200
 
     @app.route('/cursos', methods=['POST'])
     def create_curso():
@@ -60,26 +41,3 @@ def init_routes(app):
     def get_cursos():
         cursos = load_json(cursos_file)
         return jsonify(cursos), 200
-
-    @app.route('/cursos/<int:id>', methods=['PUT'])
-    def update_curso(id):
-        data = request.get_json()
-        cursos = load_json(cursos_file)
-        curso = next((c for c in cursos if c['id'] == id), None)
-        if curso is None:
-            return jsonify({"message": "Curso não encontrado!"}), 404
-
-        curso['nome'] = data['nome']
-        save_json(cursos_file, cursos)
-        return jsonify({"message": "Curso atualizado com sucesso!"}), 200
-
-    @app.route('/cursos/<int:id>', methods=['DELETE'])
-    def delete_curso(id):
-        cursos = load_json(cursos_file)
-        curso = next((c for c in cursos if c['id'] == id), None)
-        if curso is None:
-            return jsonify({"message": "Curso não encontrado!"}), 404
-
-        cursos.remove(curso)
-        save_json(cursos_file, cursos)
-        return jsonify({"message": "Curso deletado com sucesso!"}), 200
